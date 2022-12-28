7eb680701056a3c64e946b31e24603c6ba056d8e
Florian Sagolla <fsgl1@gmx.de>
2022-12-28 14:16:19 +0100

    title in coll names 3

--- tests/test_scrape_web.py
+++ tests/test_scrape_web.py
@@ -17,7 +17,7 @@ from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 from hostelprices.utils import Utils
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 import numpy as np
 
@@ -68,7 +68,7 @@ class Test(unittest.TestCase):
     
     def test_loop(self):
         city_list = ['Lisbon']
-        date_from_list = [datetime(2023, 1, 5)]
+        date_from_list = [datetime.today()+timedelta(days=10)]
         duration_list = [2]
         max_pages = 2
 
