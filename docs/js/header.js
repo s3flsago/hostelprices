bd157d28782791e65a64c37d28e303a556d1ce3d
Florian Sagolla <fsgl1@gmx.de>
2023-01-05 12:20:39 +0100

    automizing header

---
+++ docs/js/header.js
@@ -0,0 +1,11 @@
+
+headerbanner.innerHTML = `
+    <div class="banner">
+        <h1 class="banner-head">
+            hostelmonitor. <br>
+            get&nbsp;more&nbsp;out&nbsp;of&nbsp;your&nbsp;stay.
+        </h1>
+    </div >
+    `
+    ;
+
